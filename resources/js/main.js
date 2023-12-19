5976732b3ab0312ce6e2b3b044b5326d27ea39e6
DesignRuse <76643127+DesignRuse@users.noreply.github.com>
2023-12-19 21:34:31 +0530

    Add files via upload

---
+++ resources/js/main.js
@@ -0,0 +1,28 @@
+﻿
+VanillaTilt.init(document.querySelectorAll(".link-btn"), {
+	max: 10,
+	speed: 10,
+	glare: true,
+	"max-glare": 1
+});
+
+window.addEventListener('DOMContentLoaded', function () {
+
+	var copyUrls = document.getElementsByClassName('copy-url');
+
+	for (var i = 0; i < copyUrls.length; i++) {
+		copyUrls[i].addEventListener('click', async function (e) {
+			e.preventDefault();
+
+			var link = this.getAttribute('link');
+			console.log(link);
+			try {
+				await navigator.clipboard.writeText(link);
+				alert("Copied the link: " + link);
+			} catch (err) {
+				console.error(err);
+			}
+		});
+	}
+});
+
